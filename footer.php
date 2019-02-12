03d8ee59b7304a0f551b9b88e0f40bc10f518cbc
matsumoto shintaro <shiratama8942@gmail.com>
2019-02-12 09:39:51 +0900

    footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+  <footer id="contact">
+    <div class="contact-content">
+      <h2>Contact</h2>
+        <div class="enquiry">
+            <?php echo do_shortcode('[contact-form-7 id="8" title="コンタクトフォーム 1"]'); ?>
+        </div>
+    </div>
+    <!-- .contact-content -->
+    <p class="copyright">©️ 2019 shiro-8.com</p>
+  </footer>
+  <?php wp_footer(); ?>
+  </div>
+</body>
+
+</html>
